04fb474c59dcd9b303a1938154590e0b54e27947
aslan.varoqua@gmail.com <aslan.varoqua@gmail.com>
2018-05-28 03:20:16 -0600

    check point

--- functions/getobjects.js
+++ functions/getobjects.js
@@ -5,28 +5,32 @@
  * @private
  * @return {object} cosmicApi
  */
-const _cosmic  = require("cosmicjs")(); //
+const cosmic  = require("cosmicjs")(); //
+
+// TO DO : MOVE TO CONFIG
+const _slug = { name : "duas-americas-backend-content"};
 
 /**
  * gets bucket from cosmicjs
  * @private
- * @param {string} name - name of bucket slug
  * @return {object} bucket
  */
-const _bucket  = (name) => {
-    return _cosmic.bucket({slug: name});
+const _bucket  = () => {
+    return cosmic.bucket({slug: _slug.name});
 };
 /**
  * creates the response and calls back
  * @private
  * @constructor
- * @param {list} objects - A list of objects
+ * @param {list} apiResponse - A list of objects
  * @return {object} res = http response
  */
-const _fmt = (objects) => {
+const _fmt = (apiResponse) => {
+    console.log(apiResponse.objects[3]);
+    // TO DO: create formatters for each object
     return {
         statusCode: 200,
-        body: objects
+        body: apiResponse.objects[3]
     };
 };
 
@@ -38,7 +42,7 @@ const _fmt = (objects) => {
  * @return {object} fmt - http resp
  */
 const _process = function() {
-    _bucket("duas-americas-backend-content")
+    _bucket()
         .getObjects()
         .then(_fmt)
         .catch(console.log);
@@ -53,9 +57,8 @@ const _process = function() {
  * @param {function} cb
  * @returns {Promise}
  */
-module.exports.handler = (evt,ctx,cb) => {
-    _bucket
-        .getObjects()
-        .then(cb(_process))
-        .catch(console.log)
+exports.handler = (evt,ctx,cb) => {
+    cb(null,_process());
 };
+
+_process();
--- lambda-src/getobjects.js
+++ lambda-src/getobjects.js
@@ -5,28 +5,32 @@
  * @private
  * @return {object} cosmicApi
  */
-const _cosmic  = require("cosmicjs")(); //
+const cosmic  = require("cosmicjs")(); //
+
+// TO DO : MOVE TO CONFIG
+const _slug = { name : "duas-americas-backend-content"};
 
 /**
  * gets bucket from cosmicjs
  * @private
- * @param {string} name - name of bucket slug
  * @return {object} bucket
  */
-const _bucket  = (name) => {
-    return _cosmic.bucket({slug: name});
+const _bucket  = () => {
+    return cosmic.bucket({slug: _slug.name});
 };
 /**
  * creates the response and calls back
  * @private
  * @constructor
- * @param {list} objects - A list of objects
+ * @param {list} apiResponse - A list of objects
  * @return {object} res = http response
  */
-const _fmt = (objects) => {
+const _fmt = (apiResponse) => {
+    console.log(apiResponse.objects[3]);
+    // TO DO: create formatters for each object
     return {
         statusCode: 200,
-        body: objects
+        body: apiResponse.objects[3]
     };
 };
 
@@ -38,7 +42,7 @@ const _fmt = (objects) => {
  * @return {object} fmt - http resp
  */
 const _process = function() {
-    _bucket("duas-americas-backend-content")
+    _bucket()
         .getObjects()
         .then(_fmt)
         .catch(console.log);
@@ -54,5 +58,7 @@ const _process = function() {
  * @returns {Promise}
  */
 exports.handler = (evt,ctx,cb) => {
-    cb(_process())
+    cb(null,_process());
 };
+
+_process();
